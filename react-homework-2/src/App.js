8e8170a93c14525ff7bb4698bcc606a113f351d3
TarasKondratiuk <taraskondratjuk@.gmail.com>
2020-12-07 17:11:35 +0200

    react-homework-2---posts

---
+++ react-homework-2/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import "./App.css"
+import AllPosts from "./components/AllPosts";
+
+
+class App extends Component {
+
+    render() {
+
+        return (
+
+            <div>
+                <AllPosts/>
+            </div>
+        );
+    }
+}
+
+export default App;
